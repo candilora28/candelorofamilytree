ff5102db24b566f419ecbbfaf62c432fa43c390f
candilora28 <80646833+candilora28@users.noreply.github.com>
2025-07-26 11:15:12 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/candelorofamilytree/', // 👈 Replace this with your GitHub repo name
   plugins: [react()],
 })
